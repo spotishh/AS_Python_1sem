6a55d8a0b19a7905e06ef29a9c2f605e9a2f3fcb
spotishh <spotish23@gmail.com>
2025-10-28 14:27:56 +0300

    Update main.py

--- 1.4lab/main.py
+++ 1.4lab/main.py
@@ -1,2 +1,12 @@
 if __name__ == "__main__":
     pass
+nums = [7, 3, 10, 5, 8, 2, 9]
+
+# находим индексы минимального и максимального элементов
+i1 = nums.index(min(nums))
+i2 = nums.index(max(nums))
+
+# считаем сумму элементов между ними, включая оба
+result = sum(nums[min(i1, i2) : max(i1, i2) + 1])
+
+print(result)
