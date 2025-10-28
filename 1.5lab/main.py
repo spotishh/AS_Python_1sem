49be2865199c5e47c6ad43997d94157d500df364
spotishh <spotish23@gmail.com>
2025-10-28 14:21:42 +0300

    Add filtering and sorting of numbers in main.py
    Filter numbers divisible by 5 and sort them in descending order.

--- 1.5lab/main.py
+++ 1.5lab/main.py
@@ -1,2 +1,13 @@
 if __name__ == "__main__":
     pass
+nums = [3, 10, 25, 7, 40, 13, 5]
+
+# выбираем числа, которые делятся на 5 без остатка
+res = [x for x in nums if x % 5 == 0]
+
+# сортируем по убыванию
+res.sort(reverse=True)
+
+print(res)
+
+
