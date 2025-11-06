690fa9e85e0606baf641c606a699f77e85b59883
spotishh <spotish23@gmail.com>
2025-11-06 12:32:08 +0300

    Update main.py

--- 1.3lab/main.py
+++ 1.3lab/main.py
@@ -1,2 +1,11 @@
 if __name__ == "__main__":
     pass
+
+n = int(input("n: "))
+x = float(input("x (|x| < 1): "))
+
+s = 0.0
+for k in range(1, n + 1):
+    s += ((-1) ** (k - 1)) * (x ** k) / k
+
+print(s)
