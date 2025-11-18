ade70c1db1e9e8b8280f2d0603949186165a6992
spotishh <spotish23@gmail.com>
2025-11-18 12:04:56 +0300

    Update main.py

--- 2.2lab/main.py
+++ 2.2lab/main.py
@@ -1,2 +1,12 @@
 if __name__ == "__main__":
     pass
+def InvertDigits(K):
+    s = str(K)
+    perevernutaya = s[::-1]
+    result = int(perevernutaya)
+    return result
+
+print(InvertDigits(123))
+print(InvertDigits(100))
+print(InvertDigits(7))
+print(InvertDigits(98765))
